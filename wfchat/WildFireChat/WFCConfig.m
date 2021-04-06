83a10ffd94fb7b925bb8c42c56d1e7fd9f072f2b
kenny67 <282716545@qq.com>
2021-04-06 11:13:10 +0900

    init ToMe app

--- wfchat/WildFireChat/WFCConfig.m
+++ wfchat/WildFireChat/WFCConfig.m
@@ -10,18 +10,25 @@
 
 //可以是IP，可以是域名，如果是域名的话只支持主域名或www域名或im或imtest的二级域名，其它二级域名不支持！
 //例如：example.com或www.example.com或im.example.com或imtest.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。如果是专业版必须用域名，社区版建议也用域名。
-NSString *IM_SERVER_HOST = @"wildfirechat.cn";
+NSString *IM_SERVER_HOST = @"im.naifen360.com";
+//NSString *IM_SERVER_HOST = @"40.76.70.83";
+//NSString *IM_SERVER_HOST = @"192.168.3.7";
 
 
 // App Server默认使用的是8888端口，替换为自己部署的服务时需要注意端口别填错了
 // 正式商用时，建议用https，确保token安全
-NSString *APP_SERVER_ADDRESS = @"http://wildfirechat.cn:8888";
-//NSString *APP_SERVER_ADDRESS = @"https://app.wildfirechat.cn";
+NSString *APP_SERVER_ADDRESS = @"http://app.naifen360.com:8888";
+//NSString *APP_SERVER_ADDRESS = @"http://40.76.70.83:8888";
+// NSString *APP_SERVER_ADDRESS = @"http://192.168.3.7:8888";
+
 
 NSString *ICE_ADDRESS = @"turn:turn.wildfirechat.cn:3478";
 NSString *ICE_USERNAME = @"wfchat";
 NSString *ICE_PASSWORD = @"wfchat";
 
 //用户协议和隐私政策，上线前请替换成您自己的内容
-NSString *USER_PRIVACY_URL = @"https://www.wildfirechat.cn/wildfirechat_user_privacy.html";
-NSString *USER_AGREEMENT_URL = @"https://www.wildfirechat.cn/wildfirechat_user_agreement.html";
+//NSString *USER_PRIVACY_URL = @"https://www.wildfirechat.cn/wildfirechat_user_privacy.html";
+//NSString *USER_AGREEMENT_URL = @"https://www.wildfirechat.cn/wildfirechat_user_agreement.html";
+
+NSString *USER_PRIVACY_URL = @"https://www.naifen360.com/user_privacy.html";
+NSString *USER_AGREEMENT_URL = @"https://www.naifen360.com/user_agreement.html";
